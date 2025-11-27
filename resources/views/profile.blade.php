31e787eef03fbeb9ebe358dcfdd6c3734de3a120
Dimas Dharma Setiawan <90738799+itsdrsw@users.noreply.github.com>
2025-11-27 11:47:26 +0700

    Fix asset path for style.css in profile view

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -7,7 +7,7 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
 </head>
 
 <body>
